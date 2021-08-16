da465e67784767a3abeae3fd10eba408820119a2
Tomasz Oleksa <84052823+toleksa@users.noreply.github.com>
2021-08-16 20:42:05 +0200

    Update install-prometheus.sh

--- install-prometheus.sh
+++ install-prometheus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#helm install promi prometheus-community/prometheus --set server.persistentVolume.storageClass=longhorn,alertmanager.persistentVolume.storageClass=longhorn
+#helm install prometheus prometheus-community/prometheus --set server.persistentVolume.storageClass=longhorn,alertmanager.persistentVolume.storageClass=longhorn
 
-helm install promi bitnami/kube-prometheus --set prometheus.persistence.enabled=true,alertmanager.persistence.enabled=true
+helm install prometheus bitnami/kube-prometheus --set prometheus.persistence.enabled=true,alertmanager.persistence.enabled=true
 
