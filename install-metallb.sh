a1f69e2a0741134c778dfef508d481fb2d4238e1
toleksa <toleksa@gmail.com>
2021-09-07 21:52:32 +0000

    added configmap

--- install-metallb.sh
+++ install-metallb.sh
@@ -5,3 +5,5 @@ set -euo pipefail
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
 
+kubectl apply -f metallb-configmap.yaml
+
