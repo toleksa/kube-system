4e5ac17ccbda48c8080beac62c424363f09498a7
toleksa <toleksa@gmail.com>
2021-09-10 21:45:12 +0000

    global installer

---
+++ install-all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+curl -sfL https://get.rke2.io | sh -
+systemctl enable rke2-server.service
+systemctl start rke2-server.service
+
+echo "export PATH=$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
+echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
+
+./install-longhorn.sh
+./install-metallb.sh
+./install-helm.sh
+./install-externaldns.sh
+./install-monitoring.sh
+
+
