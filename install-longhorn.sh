d4d8e8c045577ea6b7e1c3232631ca9f77e2102d
toleksa <toleksa@gmail.com>
2021-09-13 21:55:17 +0000

    longer wait

--- install-longhorn.sh
+++ install-longhorn.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.1.2/deploy/longhorn.yaml
 
-echo 'sleeping 30s to propagate (if next step fails, try again in a moment'
-sleep 30s
+echo 'sleeping 90s to propagate (if next step fails, try again in a moment'
+sleep 90s
 
 kubectl patch storageclass longhorn -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
