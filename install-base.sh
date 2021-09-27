3513ff51f80c5e5931e88b00954ae31ac081df1c
toleksa <toleksa@gmail.com>
2021-09-27 16:17:59 +0000

    custom prompt

--- install-base.sh
+++ install-base.sh
@@ -8,6 +8,10 @@ systemctl start rke2-server.service
 
 echo "export PATH=$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
 echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
+echo "alias k=\"kubectl -n \$NAMESPACE \"" >> ~/.bashrc
+echo "namespace(){ export NAMESPACE=\"\$1\"; }" >> ~/.bashrc
+echo "prompt(){ PS1='\[\033[01;32m\]\u@\h\[\033[00m\](\${NAMESPACE:--}):\[\033[01;34m\]\w\[\033[00m\]$ ' ; }" >> ~/.bashrc
+echo "PROMPT_COMMAND=prompt" >> ~/.bashrc
 
 set +u
 . ~/.bashrc
