423780e98f9fe9906fd795a7fee629789a71d2ef
toleksa <toleksa@gmail.com>
2021-09-29 11:22:51 +0000

    namespace -> ns

--- install-base.sh
+++ install-base.sh
@@ -9,7 +9,7 @@ systemctl start rke2-server.service
 echo "export PATH=\$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
 echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
 echo "k(){ kubectl -n \${NAMESPACE:-default} \"\$@\"; }" >> ~/.bashrc
-echo "namespace(){ export NAMESPACE=\"\$1\"; }" >> ~/.bashrc
+echo "ns(){ export NAMESPACE=\"\$1\"; }" >> ~/.bashrc
 echo "prompt(){ PS1='\[\033[01;32m\]\u@\h\[\033[00m\](\${NAMESPACE:--}):\[\033[01;34m\]\w\[\033[00m\]\\$ ' ; }" >> ~/.bashrc
 echo "PROMPT_COMMAND=prompt" >> ~/.bashrc
 
