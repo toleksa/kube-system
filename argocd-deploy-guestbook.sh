2435d8cf6f5b2328256fe83f9274fe4e2253514a
toleksa <toleksa@gmail.com>
2021-10-08 08:35:21 +0000

    separate guestbook app

--- argocd-deploy-guestbook.sh
+++ argocd-deploy-guestbook.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 kubectl create namespace guestbook
 
-curl -kSs https://raw.githubusercontent.com/kubernetes/examples/master/guestbook/all-in-one/guestbook-all-in-one.yaml -o guestbook_app.yaml
+curl -kSs https://raw.githubusercontent.com/kubernetes/examples/master/guestbook/all-in-one/guestbook-all-in-one.yaml -o example-guestbook/guestbook_app.yaml
 
 kubectl apply -f argocd-questbook.yaml
 
