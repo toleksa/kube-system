11bbdd350998023fbf8198807beb1d330cf5fde7
toleksa <toleksa@gmail.com>
2021-10-20 12:16:29 +0000

    bash completion

--- install-bash.sh
+++ install-bash.sh
@@ -10,6 +10,8 @@ echo "prompt(){ PS1='\[\033[01;32m\]\u@\h\[\033[00m\](\${NAMESPACE:--}):\[\033[0
 echo "PROMPT_COMMAND=prompt" >> ~/.bashrc
 echo "alias king='kubectl get ingress -ALL'" >> ~/.bashrc
 echo "alias kapp='kubectl get app -ALL'" >> ~/.bashrc
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'complete -F __start_kubectl k' >>~/.bashrc
 
 set +u
 . ~/.bashrc
