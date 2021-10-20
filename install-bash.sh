4a33b4daf346ba1e53c5f72608fc9d64258ab6fc
toleksa <toleksa@gmail.com>
2021-10-20 10:32:23 +0000

    king and kapp

--- install-bash.sh
+++ install-bash.sh
@@ -8,6 +8,8 @@ echo "k(){ kubectl -n \${NAMESPACE:-default} \"\$@\"; }" >> ~/.bashrc
 echo "ns(){ export NAMESPACE=\"\$1\"; }" >> ~/.bashrc
 echo "prompt(){ PS1='\[\033[01;32m\]\u@\h\[\033[00m\](\${NAMESPACE:--}):\[\033[01;34m\]\w\[\033[00m\]\\$ ' ; }" >> ~/.bashrc
 echo "PROMPT_COMMAND=prompt" >> ~/.bashrc
+echo "alias king='kubectl get ingress -ALL'" >> ~/.bashrc
+echo "alias kapp='kubectl get app -ALL'" >> ~/.bashrc
 
 set +u
 . ~/.bashrc
