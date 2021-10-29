ce5061fd0720347aa366b64666e8146ddd8a7d32
toleksa <toleksa@gmail.com>
2021-10-29 08:58:17 +0200

    kall alias

--- install-bash.sh
+++ install-bash.sh
@@ -10,6 +10,7 @@ echo "prompt(){ PS1='\[\033[01;32m\]\u@\h\[\033[00m\](\${NAMESPACE:--}):\[\033[0
 echo "PROMPT_COMMAND=prompt" >> ~/.bashrc
 echo "alias king='kubectl get ingress -ALL'" >> ~/.bashrc
 echo "alias kapp='kubectl get app -ALL'" >> ~/.bashrc
+echo "alias kall='kubectl get all -ALL'" >> ~/.bashrc
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
 
