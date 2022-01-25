2573a408335d3e71003a7894df0ca0f446f50b6b
toleksa <toleksa@gmail.com>
2022-01-25 11:02:10 +0200

    red prompt for root

--- install-bash.sh
+++ install-bash.sh
@@ -6,7 +6,7 @@ echo "export PATH=\$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
 echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
 echo "k(){ kubectl -n \${NAMESPACE:-default} \"\$@\"; }" >> ~/.bashrc
 echo "ns(){ export NAMESPACE=\"\$1\"; }" >> ~/.bashrc
-echo "prompt(){ PS1='\[\033[01;32m\]\u@\h\[\033[00m\](\${NAMESPACE:--}):\[\033[01;34m\]\w\[\033[00m\]\\$ ' ; }" >> ~/.bashrc
+echo "prompt(){ PS1='\[\033[01;31m\]\u@\h\[\033[00m\](\${NAMESPACE:--}):\[\033[01;34m\]\w\[\033[00m\]\\$ ' ; }" >> ~/.bashrc
 echo "PROMPT_COMMAND=prompt" >> ~/.bashrc
 echo "alias king='kubectl get ingress -ALL'" >> ~/.bashrc
 echo "alias kapp='kubectl get app -ALL'" >> ~/.bashrc
