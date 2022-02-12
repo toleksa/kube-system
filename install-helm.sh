c7a95ffa0ed96fc158dbb90e73dcf5ef2fd290e4
toleksa <toleksa@gmail.com>
2022-02-12 13:37:48 +0100

    fix

--- install-helm.sh
+++ install-helm.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+. ~/.bashrc
+
 echo "installing helm"
 curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add helm https://charts.helm.sh/stable
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm repo update
+
+helm repo list
 
