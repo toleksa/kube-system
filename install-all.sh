559b4b24c4a87e2f97bb375e10778835058cf638
toleksa <toleksa@gmail.com>
2022-02-22 09:33:38 +0100

    init

---
+++ install-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/toleksa/kube-system/main/install-rke2.sh | bash
+curl https://raw.githubusercontent.com/toleksa/kube-system/main/install-bash.sh | bash
+curl https://raw.githubusercontent.com/toleksa/kube-system/main/install-helm.sh | bash
+curl https://raw.githubusercontent.com/toleksa/kube-system/main/install-argo.sh | bash
+
