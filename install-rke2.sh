63bf31e0cbb401d86f53d0e5b68cb8d5d8d7d6f8
toleksa <toleksa@gmail.com>
2022-05-12 10:44:43 +0200

    rke2 agent mode

--- install-rke2.sh
+++ install-rke2.sh
@@ -5,26 +5,24 @@ echo "START install-rke2.sh"
 # hack for stupid ubuntu .bashrc which doesn't work on noninteractive sessions
 [ -z "$PS1" ] && export PS1="> "
 
-#install rke2
-curl -sfL https://get.rke2.io | sh -
-systemctl enable rke2-server.service
-systemctl start rke2-server.service
+MODE=""
+case $1 in
+  agent | worker)
+    MODE="agent"
+    ;;
 
-echo "export PATH=\$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
-echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
+  *)
+    MODE="server"
+    ;;
+esac
 
-. ~/.bashrc
+echo "MODE: $MODE"
 
-echo "PATH:$PATH"
-echo "KUBECONFIG: $KUBECONFIG"
-echo "kubectl: `which kubectl`"
-
-#check if kubectl installed
-kubectl &> /dev/null
-if [ $? -ne 0 ]; then
-  echo "ERR: check kubectl installation"
-  exit 1
-fi 
+if [ "$MODE" == "server" ]; then
+  #install rke2
+  curl -sfL https://get.rke2.io | sh -
+  systemctl enable rke2-server.service
+  systemctl start rke2-server.service
 
 kubectl apply -f - <<EOF
 apiVersion: helm.cattle.io/v1
@@ -42,6 +40,34 @@ spec:
         enabled: true
         type: LoadBalancer
 EOF
+fi
+
+if [ "$MODE" == "agent" ]; then
+  if [ ! -f /etc/rancher/rke2/config.yaml ]; then
+    echo "ERR: /etc/rancher/rke2/config.yaml not found - skipping RKE2 installation"
+  else  
+    #install rke2
+    curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" sh -
+    systemctl enable rke2-agent.service
+    systemctl start rke2-agent.service
+  fi
+fi
+
+echo "export PATH=\$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
+echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
+
+. ~/.bashrc
+
+echo "PATH:$PATH"
+echo "KUBECONFIG: $KUBECONFIG"
+echo "kubectl: `which kubectl`"
+
+#check if kubectl installed
+kubectl &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "ERR: check kubectl installation"
+  exit 1
+fi 
 
 echo "END install-rke2.sh"
 
