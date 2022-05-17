e569c4a0d0a8579e4a92568b85272f24b9fa228a
toleksa <toleksa@gmail.com>
2022-05-17 14:12:07 +0200

    reorder

--- install-rke2.sh
+++ install-rke2.sh
@@ -23,7 +23,35 @@ if [ "$MODE" == "server" ]; then
   curl -sfL https://get.rke2.io | sh -
   systemctl enable rke2-server.service
   systemctl start rke2-server.service
+fi
 
+if [ "$MODE" == "agent" ]; then
+  if [ ! -f /etc/rancher/rke2/config.yaml ]; then
+    echo "ERR: /etc/rancher/rke2/config.yaml not found - skipping RKE2 installation"
+  else  
+    #install rke2
+    curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" sh -
+    systemctl enable rke2-agent.service
+    systemctl start rke2-agent.service
+  fi
+fi
+  
+echo "export PATH=\$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
+echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
+. ~/.bashrc
+
+echo "PATH:$PATH"
+echo "KUBECONFIG: $KUBECONFIG"
+echo "kubectl: `which kubectl`"
+
+#check if kubectl installed
+kubectl &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "ERR: check kubectl installation"
+  exit 1
+fi 
+
+if [ "$MODE" == "server" ]; then
 # check by: k get helmchartconfig.helm.cattle.io/rke2-ingress-nginx -o yaml
 kubectl apply -f - <<EOF
 apiVersion: helm.cattle.io/v1
@@ -42,33 +70,6 @@ spec:
         type: LoadBalancer
 EOF
 fi
-
-if [ "$MODE" == "agent" ]; then
-  if [ ! -f /etc/rancher/rke2/config.yaml ]; then
-    echo "ERR: /etc/rancher/rke2/config.yaml not found - skipping RKE2 installation"
-  else  
-    #install rke2
-    curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE="agent" sh -
-    systemctl enable rke2-agent.service
-    systemctl start rke2-agent.service
-  fi
-fi
-
-echo "export PATH=\$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
-echo "export KUBECONFIG=/etc/rancher/rke2/rke2.yaml" >> ~/.bashrc
-
-. ~/.bashrc
-
-echo "PATH:$PATH"
-echo "KUBECONFIG: $KUBECONFIG"
-echo "kubectl: `which kubectl`"
-
-#check if kubectl installed
-kubectl &> /dev/null
-if [ $? -ne 0 ]; then
-  echo "ERR: check kubectl installation"
-  exit 1
-fi 
 
 echo "END install-rke2.sh"
 
