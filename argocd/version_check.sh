4b6c61d56702895dae14ecbe6fd73ca5a85e8c5a
toleksa <toleksa@gmail.com>
2022-08-14 13:42:32 +0200

    relocate

---
+++ argocd/version_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd templates
+
+for APP in "argo-cd/argo-cd" "bitnami/external-dns" "longhorn/longhorn" "bitnami/metallb" "monitoring/kube-prometheus-stack"; do
+    echo "=====$APP"
+    CHART=`echo $APP | gawk -F"/" '{ print $2 }'`
+    HELM_VER=`helm search repo $APP | tail -n 1 | gawk '{ print $2 }'`
+    GIT_VER=`grep targetRevision ${CHART}.yaml | grep -v \# | gawk '{ print $2 }'`
+    echo "$APP $HELM_VER $GIT_VER"
+done
+
