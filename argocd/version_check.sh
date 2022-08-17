1fb1401d2dc3a6c296f166c895314838cd876045
toleksa <toleksa@gmail.com>
2022-08-17 09:13:55 +0200

    formatting

--- argocd/version_check.sh
+++ argocd/version_check.sh
@@ -5,7 +5,6 @@ cd templates
 for APP in "argo-cd/argo-cd" "bitnami/external-dns" "longhorn/longhorn" "bitnami/metallb" "prometheus-community/kube-prometheus-stack"; do
     COLOR='\033[00m'
     SUFFIX=''
-    echo "=====$APP"
     CHART=`echo $APP | gawk -F"/" '{ print $2 }'`
     HELM_VER=`helm search repo $APP | tail -n 1 | gawk '{ print $2 }'`
     GIT_VER=`grep targetRevision ${CHART}.yaml | grep -v \# | gawk '{ print $2 }'`
