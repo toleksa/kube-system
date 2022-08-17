fcb56080ea2a4ba58c85006e5ec49b91f47364c4
toleksa <toleksa@gmail.com>
2022-08-17 09:02:30 +0200

    colors

--- argocd/version_check.sh
+++ argocd/version_check.sh
@@ -3,10 +3,13 @@
 cd templates
 
 for APP in "argo-cd/argo-cd" "bitnami/external-dns" "longhorn/longhorn" "bitnami/metallb" "monitoring/kube-prometheus-stack"; do
+    COLOR='\033[00m'
     echo "=====$APP"
     CHART=`echo $APP | gawk -F"/" '{ print $2 }'`
     HELM_VER=`helm search repo $APP | tail -n 1 | gawk '{ print $2 }'`
     GIT_VER=`grep targetRevision ${CHART}.yaml | grep -v \# | gawk '{ print $2 }'`
-    echo "$APP $HELM_VER $GIT_VER"
+    if [ "$HELM_VER" != "$GIT_VER" ]; then
+      COLOR='\033[01;31m'
+    fi
+    echo -e "$APP ${COLOR}${HELM_VER}\033[00m $GIT_VER"
 done
-
