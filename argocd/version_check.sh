f3225603b0aa4375de5090dbc16a4b2720b082a3
toleksa <toleksa@gmail.com>
2022-08-17 09:03:26 +0200

    suffix for grep

--- argocd/version_check.sh
+++ argocd/version_check.sh
@@ -4,12 +4,14 @@ cd templates
 
 for APP in "argo-cd/argo-cd" "bitnami/external-dns" "longhorn/longhorn" "bitnami/metallb" "monitoring/kube-prometheus-stack"; do
     COLOR='\033[00m'
+    SUFFIX=''
     echo "=====$APP"
     CHART=`echo $APP | gawk -F"/" '{ print $2 }'`
     HELM_VER=`helm search repo $APP | tail -n 1 | gawk '{ print $2 }'`
     GIT_VER=`grep targetRevision ${CHART}.yaml | grep -v \# | gawk '{ print $2 }'`
     if [ "$HELM_VER" != "$GIT_VER" ]; then
       COLOR='\033[01;31m'
+      SUFFIX="!!"
     fi
-    echo -e "$APP ${COLOR}${HELM_VER}\033[00m $GIT_VER"
+    echo -e "$APP ${COLOR}${HELM_VER}\033[00m $GIT_VER $SUFFIX"
 done
