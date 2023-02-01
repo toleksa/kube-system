49b9677128e3d7a57515cd8f2b355fe65df382d6
toleksa <toleksa@gmail.com>
2023-02-01 19:30:54 +0200

    install-bash.sh - fix kapp alias

--- install-bash.sh
+++ install-bash.sh
@@ -9,7 +9,7 @@ echo "ns(){ export NAMESPACE=\"\$1\"; }" >> ~/.bashrc
 echo "prompt(){ PS1='\[\033[01;31m\]\u@\h\[\033[00m\](\${NAMESPACE:--}):\[\033[01;34m\]\w\[\033[00m\]\\$ ' ; }" >> ~/.bashrc
 echo "PROMPT_COMMAND=prompt" >> ~/.bashrc
 echo "alias king='kubectl get ingress -A'" >> ~/.bashrc
-echo "alias kapp='kubectl get app -A'" >> ~/.bashrc
+echo "alias kapp='kubectl get applications.argoproj.io -A'" >> ~/.bashrc
 echo "alias kall='kubectl get all -A'" >> ~/.bashrc
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
