149b9b343e3fbe541bed8380da399fc815c0ba85
toleksa <toleksa@gmail.com>
2024-02-15 12:33:56 +0100

    install-bash.sh - use .bash_kube from git

--- install-bash.sh
+++ install-bash.sh
@@ -4,20 +4,14 @@ echo "START install-bash.sh"
 
 set -euo pipefail
 
+curl "https://raw.githubusercontent.com/toleksa/configs/main/.bash_kube" > ~/.bash_kube
+chmod +x ~/.bash_kube
+
 cat <<'EOF' >> ~/.bashrc
-prompt(){ PS1='\[\033[01;31m\]\u@\h\[\033[00m\]($(kubectl config view --minify | grep namespace | cut -d" " -f6)):\[\033[01;34m\]\w\[\033[00m\]\$ ' ; }
-PROMPT_COMMAND=prompt
-alias k='kubectl'
-alias king='kubectl get ingress -A'
-alias kapp='kubectl get applications.argoproj.io -A'
-alias kall='kubectl get all -A'
-source <(kubectl completion bash)
-complete -F __start_kubectl k
-
-alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config current-context ; } ; f'
-alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
+if [ -f ~/.bash_kube ]; then
+    . ~/.bash_kube
+fi
 EOF
-
 
 echo "END install-bash.sh"
 
